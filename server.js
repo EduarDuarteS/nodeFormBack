aaa474699ed1c1735d4988f17c94e47763bed1db
Eduard Duarte <eduard.duarte@hotmail.com>
2019-03-07 17:28:48 -0500

    firts commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var formidable = require("formidable");
+var util = require('util');
+
+var server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (req.method.toLowerCase() == 'post') {
+        processForm(req, res);
+        return;
+    }
+
+    res.end();
+});
+
+
+function processForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields) {
+
+      //  fields.id = 'ABC123';
+
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+
+        var data = JSON.stringify({
+            fields: fields
+        });
+
+        res.end(util.inspect({
+          fields: fields
+        }));
+
+        console.log("posted fields:\n");
+        console.log(datautil.inspect({
+          fields: fields
+        }));
+    });
+}
+
+var port = 3100;
+server.listen(port);
+console.log("server listening on port " + port);
