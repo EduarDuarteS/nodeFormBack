d2c68865e8d5fb0329bfcca580109f38355fdddc
Eduard Duarte <eduard.duarte@hotmail.com>
2019-03-22 16:55:45 -0500

    se agrega el get de languages

--- server.js
+++ server.js
@@ -2,42 +2,63 @@ var http = require('http');
 var formidable = require("formidable");
 var util = require('util');
 
-var server = http.createServer(function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+var server = http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    if (req.method.toLowerCase() == 'post') {
-        processForm(req, res);
-        return;
-    }
+  if (req.method.toLowerCase() == 'post') {
+    processForm(req, res);
+    return;
+  }
 
-    res.end();
+  if (req.method.toLowerCase() == 'get') {
+    var data = {
+      data: {
+        languages: [
+          'English',
+          'Spanish',
+          'German',
+          'Other'
+        ]
+      }
+    };
+
+    var responseData = JSON.stringify(data);
+    res.end(responseData);
+    console.log("get: ", responseData);
+    return;
+  }
+
+
+  res.end();
 });
 
 
 function processForm(req, res) {
-    var form = new formidable.IncomingForm();
 
-    form.parse(req, function (err, fields) {
+  var form = new formidable.IncomingForm();
 
-      //  fields.id = 'ABC123';
+  form.parse(req, function(err, fields) {
 
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
+    fields.id = 'ABC123';
 
-        var data = JSON.stringify({
-            fields: fields
-        });
-
-        res.end(data);
-
-        console.log("posted fields:\n");
-        console.log(data);
-        // console.log(datautil.inspect({
-        //   fields: fields
-        // }));
+    res.writeHead(200, {
+      'content-type': 'text/plain'
     });
+
+    var data = JSON.stringify({
+      fields: fields,
+      validate: 'Eduard OK'
+    });
+
+    res.end(data);
+
+    console.log("posted fields:\n");
+    console.log(data);
+    // console.log(datautil.inspect({
+    //   fields: fields
+    // }));
+  });
 }
 
 var port = 3100;
